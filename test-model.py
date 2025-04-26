5c9be918decda20a2471b368f13233156cfa510a
Basmabl <basma53125@gmail.com>
2025-04-26 03:20:59 +0200

    add  wiwiiiiiiiiiiiiiiiik

--- test-model.py
+++ test-model.py
@@ -1,15 +1,17 @@
-# test_model.py
+# test_app.py
 
 import pytest
-from app import predict_visitors  # Importation directe de la fonction depuis app.py
-from unittest.mock import patch
+from app import predict_visitors
 
+# Test de la fonction predict_visitors
 def test_predict_visitors():
-    # Simulation des entrées utilisateur
-    with patch('builtins.input', side_effect=[12, 20]):  # Simule '12' pour l'heure et '20' pour la température
-        time = float(input())  # Simule l'entrée de l'heure
-        weather = float(input())  # Simule l'entrée de la température
-        result = predict_visitors(time, weather)  # Appel de la fonction de prédiction
-        print(f"Prédiction du modèle : {result}")
-        assert result > 30 and result < 60  # Vérifie que la prédiction est dans l'intervalle attendu
+    # Cas de test avec des entrées simples
+    time = 12  # 12h
+    weather = 20  # 20°C
+    prediction = predict_visitors(time, weather)
 
+    # Vérifie que la prédiction est un nombre
+    assert isinstance(prediction, (float, int))
+
+    # Vérifie que la prédiction est dans un intervalle logique
+    assert 10 < prediction < 50
